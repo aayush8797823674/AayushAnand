ce9dc832b0688085ecf145e12f04d831471770b0
Aayush Anand <aayushanand8797@gmail.com>
2025-01-23 14:05:49 +0530

    checking if a number is odd or even

---
+++ Odd_Even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Odd_Even {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+
+            System.out.print("Enter a number:");
+            int X = sc.nextInt();
+
+            if(X%2 == 0){
+                System.out.println("Number is Even.");
+            }
+            else{
+                System.out.println("Number is Odd.");
+            }
+        }
+    }
+}
