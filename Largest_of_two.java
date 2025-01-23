dc2ee1ec801ad077cd29c247324b5b88abe66dc1
Aayush Anand <aayushanand8797@gmail.com>
2025-01-23 13:57:29 +0530

    adding largest of two number

---
+++ Largest_of_two.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Largest_of_two {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+
+            System.out.print("Input first number:");
+            int a = sc.nextInt();
+            System.out.print("Input Second number:");
+            int b = sc.nextInt();
+
+            if(a>b){
+                System.out.print("'a' is largest");
+            }
+            else if(b>a){
+                System.out.print("'b' is largest");
+            }
+            else{
+                System.out.println("Both are equal");
+            }
+        }
+    }
+}
