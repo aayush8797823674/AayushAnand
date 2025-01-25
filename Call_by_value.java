e15ebff981fcbedcdf1b8b96ef3b3f44f6ec6ab5
Aayush Anand <aayushanand8797@gmail.com>
2025-01-25 19:09:51 +0530

    Call by value

---
+++ Call_by_value.java
@@ -0,0 +1,17 @@
+public class Call_by_value{
+
+    public static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        swap(a,b);
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+    }
+}
