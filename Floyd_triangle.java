f8cf01bddff8f67ba974b0bfab74ceaa3bcf14de
Aayush Anand <aayushanand8797@gmail.com>
2025-01-27 11:57:45 +0530

    Print Floyd Traingle pattern

--- Floyd_triangle.java
+++ Floyd_triangle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Floyd_triangle {
 
-    public static void inverted_half_pyramid(int n){
+    public static void floid_triangle(int n){
 
         int k = 1;
         for(int i = 0; i < n; i++){
@@ -19,7 +19,7 @@ public class Floyd_triangle {
             System.out.print("Enter Number : ");
             int n = sc.nextInt();
             
-            inverted_half_pyramid(n);
+            floid_triangle(n);
         }
     }
 }
