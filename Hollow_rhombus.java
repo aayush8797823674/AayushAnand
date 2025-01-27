95df4968ec2654ef5e2f3c78156ae613659d5f3c
Aayush Anand <aayushanand8797@gmail.com>
2025-01-27 23:13:30 +0530

    Print Hollow Rhombus

---
+++ Hollow_rhombus.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Hollow_rhombus {
+    
+    public static void solid_rhombus(int n){
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= n; j++){
+                if(i==1 || i==n){
+                    System.out.print("* ");
+                }
+                else if(j==1 || j==n){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter Number : ");
+            int n = sc.nextInt();
+            
+            solid_rhombus(n);
+        }
+    }
+}
